07458ee9534774f0074b802d54e45ac7945aea14
grant smith <grantsmith1256@gmail.com>
2019-03-18 19:51:10 +0000

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,34 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :'/forms/homepage'
+  end
+
+  get '/signup' do
+    erb :'/forms/signup'
+  end
+
+  post '/signup' do
+
+  end
+
+  helpers do
+    private
+
+    def current_user
+      User.find_by(id: session[:id])
+    end
+
+    def is_logged_in?
+      !!current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !is_logged_in?
+        redirect '/'
+      end
+    end
+  end
+
 end
